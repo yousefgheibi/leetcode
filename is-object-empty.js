30d323c7ffd5b4357f44e336fbaaf0a2a1574d40
yousef <gheybi@optt.ca>
2024-02-15 19:24:09 +0330

    [solved] 2727. Is Object Empty

---
+++ is-object-empty.js
@@ -0,0 +1,15 @@
+// Given an object or an array, return if it is empty.
+
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+var isEmpty = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    } else if (typeof obj === 'object') {
+        return Object.keys(obj).length === 0;
+    } else {
+        return false;
+    }
+};
